d8cd0baf35ebbf336ec76063dde912ac0f000037
ahamire <gfueo3243@gmail.com>
2023-11-03 14:51:43 +0200

    l added oop consepts

--- registration.js
+++ registration.js
@@ -1,17 +1,24 @@
-const phone_number = document.getElementById('phone_number');
-
-var password = document.getElementById("password").value;
-var confirm_password = document.getElementById("confirm_password").value;
-const btn = document.querySelector(".Create_account");
-btn.addEventListener('click',function(){
-var password = document.getElementById("password").value;
-var confirm_password = document.getElementById("confirm_password").value;
-if (password != confirm_password) {
-    alert("Passwords do not match!");
-}else if(password == ''){
-        
-} 
-else if (password == confirm_password&&password.length>=8){
-    alert("You did it"); 
-    btn.type ='submit';
-}});
+class data {
+    constructor(){
+        this.phone_number = document.getElementById('phone_number');
+        this.btn = document.querySelector(".Create_account");
+    
+    }
+}
+function ekd(nd){
+nd.btn.addEventListener('click',function(){
+    var password = document.getElementById("confirm_password").value;
+    var confirm_password = document.getElementById("confirm_password").value;
+    if (password != confirm_password) {
+            alert("Passwords do not match!");
+    }else if(password == ''){
+            
+    } 
+    else if (password == confirm_password&&password.length>=8){
+        alert("You did it"); 
+        nd.btn.type ='submit';
+    }
+});
+}
+const newdata = new data();
+ekd(newdata);
